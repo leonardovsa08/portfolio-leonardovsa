1b01c232551fd5301bf5dc16ee79f7415cfd7627
leonardovsa08 <leonardovasconcelosdesa@gmail.com>
2026-05-30 23:52:19 -0300

    contact and footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,11 @@ import { EducationComponent } from "./education/education.component";
 import { SkillsComponent } from "./skills/skills.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, BannerComponent, AboutComponent, EducationComponent, SkillsComponent, ProjectsComponent, ContactComponent],
+  imports: [HeaderComponent, BannerComponent, AboutComponent, EducationComponent, SkillsComponent, ProjectsComponent, ContactComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
